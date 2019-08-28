8ea47b7d9a7f297e9eb2d4c85bc4f06c25ce07a5
bilal-chughtai <brchughtaii@gmail.com>
2019-08-28 12:28:25 +0100

    added data to graph code

--- grapher.m
+++ grapher.m
@@ -1,52 +1,18 @@
-r = 0
-l = 0
-re = 1
-m = 6
-p = 2
-sigma = 0
-mode = "rand"
-trials = 100
-
-  
-prop=1  
-mvalues=[6,7,8]
-pvalues=[5,6,7]
-
-for a = 1:size(mvalues, 2)
-    m=mvalues(a);
-    
-    for b=1:size(pvalues,2)
-        p=pvalues(b);
-        i=1;
-        disp('m')
-        disp(m)
-        disp('p')
-        disp(p)
-        output=[];
-        K=[] ;
-        time=[];
-        prop=1;
-
-        
-        while prop > 0.05
-            K=[K, i];
-            disp(i)
-            [ave_time, B, prop] = run(r, l, re, m, p, sigma, mode, i, trials);
-            output = [output, prop];
-            time = [time, ave_time];
-            disp(prop)
-            i=i+1;
-        end
-        
-        filename=strcat("tests/B", num2str(B),'r', num2str(r),'l', num2str(l),'r', num2str(r),'m', num2str(m),'p', num2str(p), 'trials', num2str(trials))
-        save(filename, "K", "output", "time");
+files=dir("tests")
+for a=1:length(files)
+    filename=files(a).name
+    if filename~="." && filename~=".."  && filename~="figs"  
+        filepath=fullfile("tests",filename)
+        load(filepath)
+        filename=char(filename)
+        filename = filename(1:end-4)
+        fig = figure
+        plot(K,output)
+        xlabel('K')
+        ylabel('Proportion Found')
+        title(filename)
+        filepath = strcat("tests/figs/",filename,".fig")
+        savefig(fig, filepath)
+        close
     end
 end
-
-
-
-x=strcat("tests/B", num2str(B),'r', num2str(r),'l', num2str(l),'r', num2str(r),'m', num2str(m),'p', num2str(p), 'trials', num2str(trials))
-x(1)
-plot(K, output)
-xlabel('K')
-ylabel('prop found')
